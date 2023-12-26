123bc0e859d6db35c489541cd6098aafecd9edca
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 11:22:28 +0000

    operator

---
+++ basic/operator.py
@@ -0,0 +1,26 @@
+#take input as 2543 and output should be 254
+
+x=2545
+print("modular=",x//10)
+
+#take input 2089 and output would be 9
+x=2089
+print("mode",2089%10)
+
+#swapping the two programm
+
+x=int(input('enter the number'))
+y=int(input("enter the another number"))
+print("before swippin x=",x ,"before swipping y=",y)
+x,y=y,x
+print("x=",x,"y=",y)
+
+#three digit from the user input
+print("enter the three number")
+x=int(input())
+print(x//100)
+
+#the middle user from input
+print("enter the input")
+x=int(input())
+print(x//10%10)
