f7339b083526ff7fcfb0b429d9ccb187d13f6656
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 06:00:48 +0000

    type

---
+++ basic/type.py
@@ -0,0 +1,17 @@
+"""type of is used to see the type of the variable it contains"""
+x=45
+print(type(45)) #here the output will be the integer 
+
+print()
+
+x=3.4
+print(type(x)) #here the output will be the float
+
+x=3+4j
+print(type(x)) #here it will be the complex number
+
+x=1
+x>3
+print(type(x>3)) #it is boolean that will tell that whether it is true or false
+
+
