8a2289ccc5fd2a992de94815437862a2d03f5080
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 10:28:05 +0000

    simple program

--- basic/input.py
+++ basic/input.py
@@ -1,7 +1,31 @@
 """input method to take user  input"""
-print("enter a number")
-a=int(input())
-b=int(input())
-c=a+b
-print(c)
-print(chr(3*10))
+# print("enter a number")
+# a=int(input())
+# b=int(input())
+# c=a+b
+# print(c)
+# print(chr(3*10))
+
+
+# user=input("enter your name:")
+
+# print(user)
+# #prog 1
+# user1=int(input("enter the first number:"))
+# user2=int(input("enter the second number:"))
+# user3=user1+user2
+# print("sum of user1 is %d and user2 is %d is %d"%(user1,user2,user1+user2))
+
+# #programm 3
+
+# r=int(input("radiius of the circle"))
+# area=3.14*r*r
+# print(area)
+# print()
+
+# x=int(input("enter the number"))
+# area=x*x
+# print("area of square is %d"%(area))
+
+x=input("enetr the caharacter you want=")
+print("the unicode of",x,"is",ord(x))
