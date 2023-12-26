426ebe2719396a367b572350113f2a2225e134dd
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 10:50:51 +0000

    warmup

---
+++ basic/calculate.py
@@ -0,0 +1,28 @@
+#simple interest
+priciple=int(input("enter the amount "))
+rate=float(input("enter the rate "))
+time=float(input("enter the time "))
+simple_interest=(priciple*rate*time)/100
+print(simple_interest)
+
+#area of the rectangle
+
+length=int(input("enetr the length:"))
+breadth=int(input("enter the breadth:"))
+area=2*(length+breadth)
+print("rectangle area","is",area)
+
+#average of three number
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+z=int(input("enter the third number"))
+print((x+y+z)/3)
+#volume of the cuboid
+x=int(input("enetr the volume of the cuboid"))
+print("the volume of the cuboid is",x*x*x)
+
+#square of the number
+
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+print( "the x to the power y is",x**y)
