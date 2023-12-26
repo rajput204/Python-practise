bd5065f7d0e1b18603b47a82da1555d6ebc7dbb7
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 08:43:03 +0000

    programms

---
+++ basic/problem.py
@@ -0,0 +1,18 @@
+"""write a python  script to print hello coder on the screen"""
+print("hello coder")
+
+print(chr(3)*10)
+
+"""write a python script to print hello coder on first line and to the world in the second line"""
+print("hello coder")
+print("to the world")
+print(chr(3)*10)
+
+#write the python script to print "hello coder " with the double quotes
+print('"hello coder"')
+print(chr(5)*10)
+#write the python scipt  to print the teacher day with double quotes and single quotes 
+print('''" 'teacher's day'" ''''')
+
+#write a python script to print \n on the screen
+print("hello\n coder")
---
+++ basic/variable.py
@@ -0,0 +1,14 @@
+#write the python script conataining vaiable with some integer value and print value of this variable
+x=89
+print(x)
+
+x="Aditya kumar"
+print(x)
+
+a,b,c,d,e=35,True,"aditya",5.46,3+4j
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+
