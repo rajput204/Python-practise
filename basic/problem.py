3bd39179187fbf21565c4bbc81cdc467ba2e10de
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-26 08:55:12 +0000

    problem

--- basic/problem.py
+++ basic/problem.py
@@ -16,3 +16,24 @@ print('''" 'teacher's day'" ''''')
 
 #write a python script to print \n on the screen
 print("hello\n coder")
+print( )
+#write a python code to get current time date in a format dd/mm/yy or yy/mm/dd
+import calendar
+yy=2024
+mm=1
+print(calendar.month(yy,mm))
+print()
+print(chr(3)*20)
+#date seperatoe
+a=1
+b=11
+c=2024
+print(a,b,c,sep='/')
+
+#module of the calendar
+
+import calendar
+print(calendar.calendar(2024))
+
+
+
