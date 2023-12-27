20a6a8e34d219a38c55d01921c728df543b5373c
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-27 12:46:33 +0000

    change

---
+++ loop/while1.py
@@ -0,0 +1,35 @@
+#to print n time on the screen taking the user input
+x=int(input("enter the number of time u want to print"))
+i=1
+while(i<=x):
+    print("aditya")
+    i+=1
+print()
+#print n natural number on the screen
+x=int(input("enter the number"))
+i=1
+print("the natural number are as follows:-")
+while(i<=x):
+    print(i)
+    i+=1
+print()
+# n natural number in reverse order
+x=int(input("reverse the number as follows"))
+while x:
+    print(x,end="")
+    x-=1
+print()
+# odd natural number
+x=int(input("enter the number"))
+i=1
+while(i<=x):
+    print(2*i-1,end=" ")
+    i+=1
+print()
+# n odd natural number in reverse order
+x=int(input("enter the number u want to reverse for the odd\n"))
+while x:
+    print(2*x-1,end=" ")
+    x-=1
+print()
+
