d60b61496e98844c73e6edf76a8e29a4416437df
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-27 09:21:15 +0000

    whileloop

---
+++ loop/while.py
@@ -0,0 +1,35 @@
+#printing the name five time
+x="aditya"
+i=1
+while(i<=5):
+    print(x)
+    i+=1
+
+#print 10 natural number
+i=1
+while(i<=10):
+    print(i)
+    i+=1
+print()
+
+#reverse order printing of the 10 natural number
+    
+i=10
+while(i>=1):
+    print(i)
+    i-=1
+print()
+
+#print first 10 odd natural number
+i=1
+while(i<=10):
+    print("odd number=",2*i+1)
+    i+=1
+print()
+#odd natural number in reverse order
+i=10
+print("reversed odd number")
+while(i>1):
+    print(2*i+1)
+    i-=1
+print()
