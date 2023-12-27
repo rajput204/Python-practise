ab23e0cd4892767b0cb25aae2f1d1f432e1089eb
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-27 12:21:16 +0000

    changes

--- loop/while.py
+++ loop/while.py
@@ -33,3 +33,38 @@ while(i>1):
     print(2*i+1)
     i-=1
 print()
+
+#print first even natural number
+i=1
+while(i<=10):
+    print(2*i,end=' ')
+    i+=1
+print()
+#even natural number in reverse order
+print("reversed even number")
+i=10
+while(i>1):
+    print(2*i,end=' ')
+    i-=1
+print()
+#even 10 even natural number 
+print("square of the natural number ")
+i=1
+while(i<=10):
+    print(i*i,end=' ')
+    i+=1
+print()
+
+print("cubes of the natural number")
+i=1
+while(i<=10):
+    print(i*i*i,end=' ')
+    i+=1
+print( )
+
+#print the table of the 5
+print("table of 5")
+i=1
+while(i<=10):
+    print("{}*{}={}".format(5,i,5*i))
+    i+=1
