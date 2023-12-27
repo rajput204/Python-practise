d084fca78ef8427ce84eb97d21e79b0ea1a70742
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-27 06:35:55 +0000

    decision problem

---
+++ control/decision.py
@@ -0,0 +1,30 @@
+# #to check whether the number is positive or not
+# x=int(input("enter a number"))
+# if x>=0:
+#     print("number is positive")
+# else:
+#     print("negative number")
+# print()
+
+# #whether number is divisible by 5
+# x=int(input("enter a number"))
+# if(x%5==0):
+#     print("divisible by 5")
+# else:
+#     print("not divisible by 5")
+# print()
+
+#to check the number is even or odd
+# x=int(input("enter the number "))
+# if(x%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+#write the python script to print two given word in dictionary order
+x=input("the first given word")
+y=input("another given word")
+if x<y:
+   print(x,y,sep='\n')
+else:
+    print(y,x,sep='\n')
