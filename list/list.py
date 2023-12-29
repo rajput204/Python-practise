fd08a30b63a1bbcc68c3b5f9664456326df649b6
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-29 08:27:13 +0000

    list

---
+++ list/list.py
@@ -0,0 +1,27 @@
+l=[78,45,6,12,3]
+print(l[::1])
+
+print(l[::-1])
+
+print(min(l))
+print(max(l))
+print(len(l))
+l1=sorted(l)
+print(l1)
+l.insert(1,99)
+print(l)
+l.append(11)
+print(l)
+l.remove(99)
+print(l)
+l.pop()
+print(l)
+l.clear()
+print(l)
+l.append(89)
+print(l)
+l.count(89)
+
+#built in methods in list are sum,sorted,min,max,len
+
+#list object method are:-append,insert,remove,pop,clear,index,count,sort,reverse
