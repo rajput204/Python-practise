64ddd4f63e41dcd9c53766f257fbc8fb0cd44282
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-29 03:03:37 +0000

    loop1

---
+++ loop/loop1.py
@@ -0,0 +1,25 @@
+
+#find the count of the given number
+x=input("enter the number")
+count=0
+for e in x:
+    count+=1
+print(count)
+#find the factorial of the number
+x=int(input())
+fact=1
+while x:
+   fact=fact*x
+   x-=1
+print(fact)
+
+#sum of the digit 
+
+#to print the binary equivalent of the number
+#the octal equivalent of the number
+
+
+
+
+
+
