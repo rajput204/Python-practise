48ac086ccea625ad851383d81168937f5f525575
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-30 04:56:06 +0000

    list

--- list/list1.py
+++ list/list1.py
@@ -1,25 +1,25 @@
-# print("enter the number seperated by comma")
-# n=input()
-# print(n)
-# l=[int(e)for e in n.split(',')]
-# print("sum=",sum(l))
+print("enter the number seperated by comma")
+n=input()
+print(n)
+l=[int(e)for e in n.split(',')]
+print("sum=",sum(l))
 
-# print("enter the number seperated by comma")
-# n=input()
-# print(n)
-# l=[int(e)for e in n.split(',')]
-# print("avg=",sum(l)/len(l))
+print("enter the number seperated by comma")
+n=input()
+print(n)
+l=[int(e)for e in n.split(',')]
+print("avg=",sum(l)/len(l))
 
-# print("enter the number seperated by comma")
-# n=input()
-# print(n)
-# l=[int(e)*int(e) for e in n.split(',')]
-# print("square=",l)
+print("enter the number seperated by comma")
+n=input()
+print(n)
+l=[int(e)*int(e) for e in n.split(',')]
+print("square=",l)
 
-# n=input()
-# l=[int(e)for e in n.split(',')]
-# l.sort(reverse=True)
-# print(l)
+n=input()
+l=[int(e)for e in n.split(',')]
+l.sort(reverse=True)
+print(l)
 
 #create a list from a given list by selecting only even places elements
 n=input()
