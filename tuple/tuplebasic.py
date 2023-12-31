3e2bf9061282de744f35ac412aeecf7f6a8b00e8
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-12-31 17:26:45 +0000

    basic

---
+++ tuple/tuplebasic.py
@@ -0,0 +1,20 @@
+# tuple is an iterable 
+# it is immutable
+# it is a class
+# it is sequenced
+# it is hasable because it has hash value
+
+
+# indexing starting from o from left hand side and from -1 from right hand side
+# built in method are 
+sum()
+sorted() #it will always will a new list
+len()
+max()
+min()
+
+
+# it only two object method 
+t=[1,2,3,4,5]
+t.index(5)
+t.count()
