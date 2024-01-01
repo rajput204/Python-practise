c9d4645e04296d91d0bcfd04af16684cd992a551
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2024-01-01 10:30:37 +0000

    dict

---
+++ dict/dict.py
@@ -0,0 +1,24 @@
+#it is an iterable
+#it is class
+#it does not contains duplicate values
+#slicing and indexing is not supported
+# it is not hasable
+#it only supports  equality operator
+
+#dict built-in method
+sum()
+sorted()
+len()
+max()
+min()
+
+#dict object method
+d={}
+d.keys()
+d.values()
+d.items()
+
+#edit the values of specific method
+d[102]="string"
+#delete
+del[102]
