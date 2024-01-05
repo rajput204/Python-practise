0ab80a6a2bf479d7072acd05a7d1ccb9e685dcab
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2024-01-05 18:38:19 +0000

    lambda

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -4,3 +4,12 @@ print(x)
 x=(lambda r:3.14*r*r)(10)
 print(x)
 
+f=(lambda n: n if n==0 or n==1 else f(n-1)+f(n-2))
+print(f(10))
+
+hcf=lambda a,b: (b if a%b==0 else hcf(a%b,b)) if a>b else(a if b%a==0 else hcf(a,b%a))
+print(hcf(12,18))
+
+x=lambda text:len(text.split(' '))
+t=("aditya a good guy")
+print(x(t))
